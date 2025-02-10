8246639547e2364d49d0040cf68b4a1ce59f50e7
Àngel Buigues Andrés <angelbuiguesandres.teulada@gmail.com>
2025-02-10 16:35:24 +0100

    d

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import GymsView from '../views/GymsView.vue'
-import ReservasView from '../views/ReservasView.vue'
+import ReservasView from '@/views/ReservasView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
